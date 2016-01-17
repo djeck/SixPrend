19f67a7976cc9d4f6a5adea7c3385e1cfbdeeb1e
djeck <djeck.det@gmail.com>
2016-01-17 19:25:59 +0100

    documentation du login

--- Game/login.h
+++ Game/login.h
@@ -12,10 +12,28 @@
 #include <SDL2/SDL_ttf.h>
 #include "inc.h"
 
+/*
+ * Gestion des evenements spécifique au login
+ * agis sur la variable globale terminer
+ */
 void eventLogin();
+/*
+ * Initialisation des ressources graphiques spécifique au rendu du login
+ * necessite la variable renderer
+ */
 void initLoginRender();
+/*
+ * Met à jour la texture du texte avec la chaine de charactere str
+ */
 void updateText(char str[]);
+/*
+ * dessine ressources graphiques spécifique au rendu du login
+ * necessite la variable renderer
+ */
 void renderLogin();
+/*
+ * Libere les ressources graphiques initialisees par initLoginRender
+ */
 void freeLoginRender();
 
 #endif
