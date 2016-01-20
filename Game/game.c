0cf9578811cd10756ac442290271b0553e91f132
djeck <djeck.det@gmail.com>
2016-01-20 21:02:03 +0100

    ajout fenetre de jeu

--- Game/game.c
+++ Game/game.c
@@ -1,5 +1,9 @@
 #include "game.h"
 
+static PickableImage choixQuit;
+static PickableImage choixBack;
+static Image Background;
+
 void eventGame()
 {
   SDL_Event event;
@@ -11,7 +15,7 @@ void eventGame()
 	    {
 	      changeStep(end);
 	    }
-	    if(collisionWithMouse(choixBack.rect,event.button.x,event.button.y))
+	    else if(collisionWithMouse(choixBack.rect,event.button.x,event.button.y))
 	    {
 	      changeStep(menu);
 	    }
@@ -31,3 +35,38 @@ void eventGame()
 	}
 }
 
+static int renderinitialised = 0;
+
+void initGameRender()
+{
+	
+	Background = createPicture(BACKGROUNDPATH,0,0,1);
+	
+	choixQuit = createPickableText("Exit",400,500,8);
+	choixBack = createPickableText("Return",100,500,8);
+	
+	renderinitialised=1;
+}
+void renderGame()
+{
+	if(renderinitialised==0)
+		return;
+	renderImage(Background);
+	renderPickableImage(choixQuit);
+	renderPickableImage(choixBack);
+}
+
+void freeGameRender()
+{
+	if(renderinitialised==0)
+	{
+	  printf("freeGameGame: ne peut pas liberer les ressources car elles n'ont pas etaient inititialisé\n");
+		return;
+	}
+	renderinitialised=0;
+	freePickableImage(choixQuit);
+	freePickableImage(choixBack);
+	freeImage(Background); // Libération de la mémoire associée à la texture
+
+	printf("freeGameGame: liberation des ressources\n");
+}
