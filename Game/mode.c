5a24095e8edfe1ee3b02bd4f383f3ec1c36cc528
djeck <djeck@djeck-13.home>
2016-02-29 21:14:31 +0100

    ajout choix ip du serveur

--- Game/mode.c
+++ Game/mode.c
@@ -5,6 +5,11 @@ static PickableImage choixStart;
 static PickableImage choixBack;
 static Image Background;
 
+static Image ipAsk;
+static PickableImage ipServer;
+
+static char ipText[100];
+
 void eventMode()
 {
     SDL_Event event;
@@ -24,6 +29,7 @@ void eventMode()
             {
                 changeStep(game);
             }
+            ipServer.select = collisionWithMouse(ipAsk.rect,event.button.x,event.button.y);
 
         }
         else if( event.type == SDL_MOUSEMOTION )
@@ -32,10 +38,33 @@ void eventMode()
             choixBack.select=collisionWithMouse(choixBack.rect,event.motion.x,event.motion.y);
             choixStart.select=collisionWithMouse(choixStart.rect,event.motion.x,event.motion.y);
         }
+        else if ( event.type == SDL_KEYUP )
+	{
+	  if ( event.key.keysym.sym == SDLK_DELETE || event.key.keysym.sym == SDLK_BACKSPACE )
+            {
+                if(ipServer.select && strlen(ipText)!=0)
+		{
+		  ipText[strlen(ipText)-1]='\0';
+		  updatePickableText(&ipServer,ipText,150,100,8);
+		}
+                printf("eventLogin: character erased\n");
+            }
+	}
         switch(event.type)
         {
         case SDL_QUIT:
             changeStep(end);
+            break;
+	case SDL_TEXTINPUT:
+	  if(ipServer.select)
+	  {
+            if(strlen(ipText)<100-1)
+            {
+                strcat(ipText, event.text.text);
+            }
+            updatePickableText(&ipServer,ipText,150,100,8);
+            printf("eventMode: ipText = %s\n",ipText);
+	  }
             break;
         }
     }
@@ -45,12 +74,15 @@ static int renderinitialised = 0;
 
 void initModeRender()
 {
-
+    strcpy(ipText,"127.0.0.1");
     Background = createPicture(BACKGROUNDPATH,0,0,1);
-
+    
     choixQuit = createPickableText("Exit",400,500,8);
     choixBack = createPickableText("Return",100,450,8);
     choixStart = createPickableText("Start game",150,200,6);
+    
+    ipAsk = createText("Server IP",20,100,8,false);
+    ipServer = createPickableText(ipText,150,100,8);
 
     renderinitialised=1;
 }
@@ -59,6 +91,10 @@ void renderMode()
     if(renderinitialised==0)
         return;
     renderImage(Background);
+    
+    renderImage(ipAsk);
+    renderPickableImage(ipServer);
+    
     renderPickableImage(choixQuit);
     renderPickableImage(choixBack);
     renderPickableImage(choixStart);
@@ -72,10 +108,12 @@ void freeModeRender()
         return;
     }
     renderinitialised=0;
-    freePickableImage(choixQuit);
+    freePickableImage(choixQuit);// Libération de la mémoire associée aux textures
+    freePickableImage(ipServer);
+    freeImage(ipAsk);
     freePickableImage(choixBack);
     freePickableImage(choixStart);
-    freeImage(Background); // Libération de la mémoire associée à la texture
+    freeImage(Background);
 
     printf("freeModeRender: liberation des ressources\n");
 }
